8f601847a8205ecbc487f4c5ec924428728a3c63
user <user@pchome-0c6a.modul.spb.ru>
2019-06-28 12:56:24 +0300

    Initial commit

---
+++ QuillAsset.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace ofilin\quill;
+
+use yii\web\AssetBundle;
+
+class QuillAsset extends AssetBundle
+{
+
+    public $theme;
+
+    public $sourcePath = '@ofilin/quill/src/dist';
+
+
+    public function registerAssetFiles($view)
+    {
+        switch ($this->theme) {
+            case Quill::THEME_SNOW:
+                $this->css = ['quill.snow.css'];
+                break;
+            case Quill::THEME_BUBBLE:
+                $this->css = ['quill.bubble.css'];
+                break;
+            default:
+                $this->css = ['quill.core.css'];
+        }
+
+        $this->js = ['quill.min.js'];
+
+        parent::registerAssetFiles($view);
+    }
+}
